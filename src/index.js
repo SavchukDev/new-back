a6633f0fe94d800ac40cc9cd1559dbb4d467322f
SavchukDev <savchuk421@gmail.com>
2026-02-27 12:31:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'https://catharsis-farm.com',
   'https://www.catharsis-farm.com',
+  'https://front-new-mo4u.onrender.com',
 ];
 
 const corsOptions = {
